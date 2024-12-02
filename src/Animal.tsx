a6783b0046b8d4e0f183fcb7e17429d1660dc798
Igor Knop <igor.knop@ufjf.br>
2024-12-02 20:20:17 -0300

    adiciona estilo inicial

--- src/Animal.tsx
+++ src/Animal.tsx
@@ -1,4 +1,19 @@
 import "./Animal.css";
-export default function () {
-  return <div className="animal">Animal</div>;
+
+interface AnimalProps {
+  icone: string;
+  nome: string;
+  peso: number;
+  extincao: boolean;
+}
+
+export default function Animal({ icone, nome, peso, extincao }: AnimalProps) {
+  return (
+    <div className="animal">
+      <div>{icone}</div>
+      <div>{nome}</div>
+      <div>{peso.toLocaleString()}kg</div>
+      <div>{extincao}</div>
+    </div>
+  );
 }
--- src/App.tsx
+++ src/App.tsx
@@ -4,7 +4,7 @@ import Animal from "./Animal";
 function App() {
   return (
     <div className="app">
-      <Animal />
+      <Animal icone="🦁" nome = "Leão" peso = {190.37} extincao={true}/>
     </div>
   );
 }
