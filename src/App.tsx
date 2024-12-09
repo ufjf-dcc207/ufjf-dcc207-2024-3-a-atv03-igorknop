1e1c8e88a0a739b70060798d0f98ba61e2c5a00b
Igor Knop <igor.knop@ufjf.br>
2024-12-09 20:17:58 -0300

    adiciona exemplo ruim de filtro

--- src/App.tsx
+++ src/App.tsx
@@ -12,16 +12,12 @@ const ANIMAIS: AnimaisTuplaType[] = [
 ];
 
 function App() {
-  const exA1: ReactNode[] = [];
-  const exB2: ReactNode[] = ANIMAIS.map((animal) => <Animal
-      key={animal[1]}
-      icone={animal[0]}
-      nome={animal[1]}
-      peso={animal[2]}
-      extincao={animal[3]}
-    />
+  const exA1: AnimaisTuplaType[] = ANIMAIS.filter(
+    (animal) => animal[2] < 200.0
   );
-
+  const exB2: AnimaisTuplaType[] = ANIMAIS.filter(
+    (animal) => animal[2] >= 200.0
+  );
   return (
     <div className="app">
       <Exibicao
@@ -29,14 +25,30 @@ function App() {
         fechamento={new Date("2024-12-06T12:00-03:00")}
         cercado="A1"
       >
-        {exA1}
+        {exA1.map((animal) => (
+          <Animal
+            key={animal[1]}
+            icone={animal[0]}
+            nome={animal[1]}
+            peso={animal[2]}
+            extincao={animal[3]}
+          />
+        ))}
       </Exibicao>
       <Exibicao
         abertura={new Date("2024-12-06T13:00:00.000-03:00")}
         fechamento={new Date("2024-12-06T17:00-03:00")}
         cercado="B1"
       >
-        {exB2}
+        {exB2.map((animal) => (
+          <Animal
+            key={animal[1]}
+            icone={animal[0]}
+            nome={animal[1]}
+            peso={animal[2]}
+            extincao={animal[3]}
+          />
+        ))}
       </Exibicao>
     </div>
   );
